4e660f327d2c2fb24384d5251471ce41a01d447e
ksmart2 <kelsey.smart.02@gmail.com>
2024-12-16 04:00:38 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -5,31 +5,28 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
+The accessibility of the public parks in
+the City of Knoxville, Tennessee within
+the different residential zones.
 """
 
 st.sidebar.title("About")
 st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
 
 # Customize page title
-st.title("Streamlit for Geospatial Applications")
+st.title("Park Accessibility for Residential Zones in Knoxville, TN")
 
 st.markdown(
     """
-    This multipage app template demonstrates various interactive web apps created using [streamlit](https://streamlit.io) and [leafmap](https://leafmap.org). It is an open-source project and you are very welcome to contribute to the [GitHub repository](https://github.com/opengeos/streamlit-map-template).
+    The goal of this project was to assess the accessibility of the public parks in the City of Knoxville, Tennessee within the different residential zones. By utilizing geographic data on parks and zoning areas, this study conducted spatial analysis methods to evaluate whether each residential zone has access to a park and to examine the overall distribution of parks throughout Knoxville. Unlike previous studies that primarily focus on general park availability, this project explores the geographic distribution of parks in relation to residential zones, with a focus on identifying any disparities in access for underserved communities within Knoxville. 
     """
 )
 
-st.header("Instructions")
+st.header("Datasets")
 
 markdown = """
-1. For the [GitHub repository](https://github.com/opengeos/streamlit-map-template) or [use it as a template](https://github.com/opengeos/streamlit-map-template/generate) for your own project.
-2. Customize the sidebar by changing the sidebar text and logo in each Python files.
-3. Find your favorite emoji from https://emojipedia.org.
-4. Add a new app to the `pages/` directory with an emoji in the file name, e.g., `1_🚀_Chart.py`.
+- [Park and Zoning Data](https://knoxplanning.org/data/open-data)
+
 
 """
 
