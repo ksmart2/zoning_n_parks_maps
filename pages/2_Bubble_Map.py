ffc321fa1cbf9fd12caf17e406e9c755d504eeb6
ksmart2 <kelsey.smart.02@gmail.com>
2024-12-16 01:19:32 -0500

    Update 2_Bubble_Map.py

--- pages/2_Bubble_Map.py
+++ pages/2_Bubble_Map.py
@@ -22,7 +22,7 @@ with st.expander("See source code"):
     with st.echo():
 
         m = leafmap.Map(center=[35.9606, -83.9207], zoom=12)
-        geojson_url = "https://raw.githubusercontent.com/ksmart2/zoning_n_parks_maps/refs/heads/main/i_counts_geom_gdf.geojson"
+        geojson_url = "https://raw.githubusercontent.com/ksmart2/zoning_n_parks_maps/refs/heads/main/i_counts_geom_gdf%20(1).geojson"
         i_counts_geom_gdf = gpd.read_file(geojson_url)
 
         for _, row in i_counts_geom_gdf.iterrows():
