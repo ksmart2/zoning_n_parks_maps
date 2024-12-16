1371e33bc7a28a5c135342f992225fd370f84c7c
ksmart2 <kelsey.smart.02@gmail.com>
2024-12-16 01:01:50 -0500

    Update and rename 2_🪟_Split_Map.py to 2_Bubble_Map.py

---
+++ pages/2_Bubble_Map.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import leafmap.foliumap as leafmap
+
+st.set_page_config(layout="wide")
+
+markdown = """
+A Streamlit map template
+<https://github.com/opengeos/streamlit-map-template>
+"""
+
+st.sidebar.title("About")
+st.sidebar.info(markdown)
+logo = "https://i.imgur.com/UbOXYAU.png"
+st.sidebar.image(logo)
+
+st.title("Interactive Bubble Map for Count of Residential Zones That Intersect A Park Location")
+
+with st.expander("See source code"):
+    with st.echo():
+
+        m = leafmap.Map(center=[35.9606, -83.9207], zoom=12)
+        i_counts_geom_gdf = "https://raw.githubusercontent.com/ksmart2/zoning_n_parks_maps/refs/heads/main/i_counts_geom_gdf.geojson"
+
+        for _, row in i_counts_geom_gdf.iterrows():
+            folium.CircleMarker(
+                location=[row.geometry.centroid.y, row.geometry.centroid.x],
+                radius=row['Count'] * 3,
+                color="green",
+                fill=True,
+                fill_color="green",
+                fill_opacity=0.6,
+                popup=f"Park: {row['NAME']}<br>Count: {row['Count']}",
+            ).add_to(m)
+
+
+m.to_streamlit(height=700)
